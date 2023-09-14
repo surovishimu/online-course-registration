ca2259c4e3fff7a3d30740e85d0d676e8e8d1794
surovishimu <surovi.shimu95@gmail.com>
2023-09-15 01:24:52 +0600

    add all the functionalities inside the selected course side bar

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,36 @@ import { useState } from 'react'
 
 function App() {
   const [selectedCourse, setSelectedCourse] = useState([]);
+  const [totalPrice, setTotalPrice] = useState([0]);
+  const [totalRemaining, setTotalRemaining] = useState(0);
+  const [totalCreditHour, setTotalCreditHour] = useState(0);
   const handleselectedCourse = course => {
-    const newCourse=[...selectedCourse,course];
-    setSelectedCourse(newCourse);
+    const isExist = selectedCourse.find(item => item.id == course.id);
+
+    let count = parseFloat(course.price);
+    let creditHour = parseInt(course.credit_hours);
+    if (isExist) {
+      return alert('Already select this course');
+    }
+    else {
+      selectedCourse.forEach(item => {
+        count = count + parseFloat(item.price);
+        creditHour = creditHour + parseInt(item.credit_hours)
+      
+      })
+      const totalRemaining = 20 - creditHour;
+      if (creditHour > 20) {
+        return alert('You can not add credit hour more then 20');
+      }else {
+        setTotalPrice(count);
+        setTotalCreditHour(creditHour);
+        setTotalRemaining(totalRemaining);
+        const newCourse = [...selectedCourse, course];
+        setSelectedCourse(newCourse);
+      }
+      
+    }
+
   }
 
   return (
@@ -17,7 +44,7 @@ function App() {
       <div className='bg-[#F3F3F3]'><Header></Header>
         <div className='md:flex'>
           <Courses handleselectedCourse={handleselectedCourse}></Courses>
-          <SelectedCourse selectedCourse={selectedCourse}></SelectedCourse>
+          <SelectedCourse selectedCourse={selectedCourse} totalPrice={totalPrice} totalCreditHour={totalCreditHour} totalRemaining={totalRemaining}></SelectedCourse>
         </div></div>
 
 
--- src/components/SelectedCourse/SelectedCourse.jsx
+++ src/components/SelectedCourse/SelectedCourse.jsx
@@ -1,17 +1,17 @@
 import PropTypes from 'prop-types';
-const SelectedCourse = ({ selectedCourse }) => {
+const SelectedCourse = ({ selectedCourse, totalPrice, totalCreditHour, totalRemaining }) => {
     return (
         <div className="md:w-1/4 pr-5 mt-10">
             <div className='card bg-base-100 shadow-xl mx-5'>
                 <div className='card-body'>
-                    <h2 className="text-[#2F80ED] text-sm font-semibold  border-b-2 py-2">Credit Hour Remaining:</h2>
+                    <h2 className="text-[#2F80ED] text-sm font-semibold  border-b-2 py-2">Credit Hour Remaining:{totalRemaining}</h2>
                     <h1 className='font-bold'>Course Name</h1>
                     {
                         selectedCourse.map(singleCourse => <li className='list-decimal text-sm text-gray-500' key={singleCourse.id} >{singleCourse.course_name}</li>)
 
                     }
-                    <h2 className=" text-sm border-y-2 py-4">Total Credit Hour :</h2>
-                    <h2 className=" text-sm ">Total Price:</h2>
+                    <h2 className=" text-sm border-y-2 py-4">Total Credit Hour :{totalCreditHour}</h2>
+                    <h2 className=" text-sm ">Total Price:{totalPrice} USD</h2>
                 </div>
             </div>
 
@@ -20,6 +20,10 @@ const SelectedCourse = ({ selectedCourse }) => {
 };
 
 SelectedCourse.propTypes = {
-    selectedCourse: PropTypes.array.isRequired
+    selectedCourse: PropTypes.array.isRequired,
+    totalPrice: PropTypes.number,
+    totalCreditHour: PropTypes.number,
+    totalRemaining: PropTypes.number
+
 }
 export default SelectedCourse;
