632c4e00d607eac2cd187f198b473ea3c18cc696
surovishimu <surovi.shimu95@gmail.com>
2023-09-14 22:47:32 +0600

    create the selected course list bar

--- src/components/SelectedCourse/SelectedCourse.jsx
+++ src/components/SelectedCourse/SelectedCourse.jsx
@@ -1,7 +1,14 @@
 const SelectedCourse = () => {
     return (
-        <div className="md:w-1/4">
-            <h1>selected course</h1>
+        <div className="md:w-1/4 pr-5 mt-10">
+            <div className="card bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="text-[#2F80ED] text-sm font-semibold  border-b-2 py-2">Credit Hour Remaining: </h2>
+                    <h2 className="text-md font-semibold border-b-2 py-4">Course Name:</h2>
+                    <h2 className=" text-sm border-b-2 pb-2">Total Credit Hour :</h2>
+                    <h2 className=" text-sm ">Total Price:</h2>
+                </div>
+            </div>
         </div>
     );
 };
